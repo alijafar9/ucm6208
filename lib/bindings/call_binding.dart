ac0e505cfdfdd6ac954e0e8bd241aea84e9daa20
alijafar9 <alialijafar9@gmail.com>
2025-07-29 15:03:36 +0300

    update

--- lib/bindings/call_binding.dart
+++ lib/bindings/call_binding.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
 import '../controllers/simple_call_controller.dart';
+import '../services/sip_service.dart';
 
 class CallBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<SipService>(() => SipService());
     Get.lazyPut<SimpleCallController>(() => SimpleCallController());
   }
 } 
